044ef2555a8096556232ff9e16c531db8442da88
Michael Zanussi <iosdevx@gmail.com>
2017-05-29 15:10:19 -0600

    add display

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel?
+    
+    var userIsInTheMiddleOfTyping = false
+    
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
-        print("\(digit) was called")
+        //print("\(digit) was called")
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display!.text!
+            display!.text = textCurrentlyInDisplay + digit
+        } else {
+            display!.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
     }
     
 }
